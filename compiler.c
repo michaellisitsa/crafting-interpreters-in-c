37ae1850ef073728ca4c8d238e30899b02106b85
Michael Lisitsa <michael@lisitsa.dev>
2025-10-17 22:04:30 +1100

    17.4 to 7 - Parsing tokens. A simple calculator works https://craftinginterpreters.com/compiling-expressions.html#parsing-prefix-expressions

--- compiler.c
+++ compiler.c
@@ -5,6 +5,9 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#ifdef DEBUG_PRINT_CODE
+#include "debug.h"
+#endif
 
 typedef struct {
 	Token current;
@@ -12,7 +15,25 @@ typedef struct {
 	bool hadError;
 	bool panicMode;
 } Parser;
-
+typedef enum {
+	PREC_NONE,
+	PREC_ASSIGNMENT, // =
+	PREC_OR,		 // or
+	PREC_AND,		 // and
+	PREC_EQUALITY,	 // == !=
+	PREC_COMPARISON, // < > <= >=
+	PREC_TERM,		 // + -
+	PREC_FACTOR,	 // * /
+	PREC_UNARY,		 // ! -
+	PREC_CALL,		 // . ()
+	PREC_PRIMARY
+} Precedence;
+typedef void (*ParseFn)();
+typedef struct {
+	ParseFn prefix;
+	ParseFn infix;
+	Precedence precedence;
+} ParseRule;
 Parser parser;
 Chunk *compilingChunk;
 
@@ -75,11 +96,142 @@ static void emitReturn() {
 	// Why a separate function for just return?
 	emitByte(OP_RETURN);
 }
-
-static void endCompiler() { emitReturn(); }
-static void expression() {
-	// something will go here
+static uint8_t makeConstant(Value value) {
+	int constant = addConstant(currentChunk(), value);
+	if (constant > UINT8_MAX) {
+		error("Too many constants in one chunk");
+		return 0;
+	}
+	return (uint8_t)constant;
 }
+
+static void emitConstant(Value value) { emitBytes(OP_CONSTANT, makeConstant(value)); }
+static void endCompiler() {
+	emitReturn();
+#ifdef DEBUG_PRINT_CODE
+	if (!parser.hadError) {
+		disassembleChunk(currentChunk(), "code");
+	}
+#endif
+}
+
+// Global variable forward declaration
+// The order of function declarations means some functions have to be forward declared
+static void expression();
+static ParseRule *getRule(TokenType type);
+static void parsePrecedence(Precedence precedence);
+
+static void expression() { parsePrecedence(PREC_ASSIGNMENT); }
+
+static void binary() {
+	TokenType operatorType = parser.previous.type;
+	ParseRule *rule = getRule(operatorType);
+	parsePrecedence((Precedence)(rule->precedence + 1));
+	switch (operatorType) {
+	case TOKEN_PLUS:
+		emitByte(OP_ADD);
+		break;
+	case TOKEN_MINUS:
+		emitByte(OP_SUBTRACT);
+		break;
+	case TOKEN_STAR:
+		emitByte(OP_MULTIPLY);
+		break;
+	case TOKEN_SLASH:
+		emitByte(OP_DIVIDE);
+		break;
+	default:
+		return; // Unreachable.
+	}
+}
+static void grouping() {
+	expression();
+	consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
+static void number() {
+	double value = strtod(parser.previous.start, NULL);
+	emitConstant(value);
+}
+static void unary() {
+	TokenType operatorType = parser.previous.type;
+	parsePrecedence(PREC_UNARY);
+	switch (operatorType) {
+	case TOKEN_MINUS:
+		emitByte(OP_NEGATE);
+		break;
+	default:
+		return;
+	}
+}
+ParseRule rules[] = {
+	[TOKEN_LEFT_PAREN] = {grouping, NULL, PREC_NONE},
+	[TOKEN_RIGHT_PAREN] = {NULL, NULL, PREC_NONE},
+	[TOKEN_LEFT_BRACE] = {NULL, NULL, PREC_NONE},
+	[TOKEN_RIGHT_BRACE] = {NULL, NULL, PREC_NONE},
+	[TOKEN_COMMA] = {NULL, NULL, PREC_NONE},
+	[TOKEN_DOT] = {NULL, NULL, PREC_NONE},
+	[TOKEN_MINUS] = {unary, binary, PREC_TERM},
+	[TOKEN_PLUS] = {NULL, binary, PREC_TERM},
+	[TOKEN_SEMICOLON] = {NULL, NULL, PREC_NONE},
+	[TOKEN_SLASH] = {NULL, binary, PREC_FACTOR},
+	[TOKEN_STAR] = {NULL, binary, PREC_FACTOR},
+	[TOKEN_BANG] = {NULL, NULL, PREC_NONE},
+	[TOKEN_BANG_EQUAL] = {NULL, NULL, PREC_NONE},
+	[TOKEN_EQUAL] = {NULL, NULL, PREC_NONE},
+	[TOKEN_EQUAL_EQUAL] = {NULL, NULL, PREC_NONE},
+	[TOKEN_GREATER] = {NULL, NULL, PREC_NONE},
+	[TOKEN_GREATER_EQUAL] = {NULL, NULL, PREC_NONE},
+	[TOKEN_LESS] = {NULL, NULL, PREC_NONE},
+	[TOKEN_LESS_EQUAL] = {NULL, NULL, PREC_NONE},
+	[TOKEN_IDENTIFIER] = {NULL, NULL, PREC_NONE},
+	[TOKEN_STRING] = {NULL, NULL, PREC_NONE},
+	[TOKEN_NUMBER] = {number, NULL, PREC_NONE},
+	[TOKEN_AND] = {NULL, NULL, PREC_NONE},
+	[TOKEN_CLASS] = {NULL, NULL, PREC_NONE},
+	[TOKEN_ELSE] = {NULL, NULL, PREC_NONE},
+	[TOKEN_FALSE] = {NULL, NULL, PREC_NONE},
+	[TOKEN_FOR] = {NULL, NULL, PREC_NONE},
+	[TOKEN_FUN] = {NULL, NULL, PREC_NONE},
+	[TOKEN_IF] = {NULL, NULL, PREC_NONE},
+	[TOKEN_NIL] = {NULL, NULL, PREC_NONE},
+	[TOKEN_OR] = {NULL, NULL, PREC_NONE},
+	[TOKEN_PRINT] = {NULL, NULL, PREC_NONE},
+	[TOKEN_RETURN] = {NULL, NULL, PREC_NONE},
+	[TOKEN_SUPER] = {NULL, NULL, PREC_NONE},
+	[TOKEN_THIS] = {NULL, NULL, PREC_NONE},
+	[TOKEN_TRUE] = {NULL, NULL, PREC_NONE},
+	[TOKEN_VAR] = {NULL, NULL, PREC_NONE},
+	[TOKEN_WHILE] = {NULL, NULL, PREC_NONE},
+	[TOKEN_ERROR] = {NULL, NULL, PREC_NONE},
+	[TOKEN_EOF] = {NULL, NULL, PREC_NONE},
+};
+
+// This is a recursive function
+// When it calls the prefix rule function, that function
+// e.g. unary, binary may call this again
+// There is a diagram about how this works on
+// https://craftinginterpreters.com/compiling-expressions.html#parsing-with-precedence
+static void parsePrecedence(Precedence precedence) {
+	advance();
+	// We get the rule for the token we're on,
+	// i.e. what its ParseFn for infix, prefix and infix precedence
+	// For Now we just need the prefix rule
+	ParseFn prefixRule = getRule(parser.previous.type)->prefix;
+	if (prefixRule == NULL) {
+		error("Expect expression.");
+		return;
+	}
+	prefixRule();
+
+	// Here we're going to call the infix function so it can generate some bytecode recursively
+	// inside 'expression'
+	while (precedence <= getRule(parser.current.type)->precedence) {
+		advance();
+		ParseFn infixRule = getRule(parser.previous.type)->infix;
+		infixRule();
+	}
+}
+static ParseRule *getRule(TokenType type) { return &rules[type]; }
 bool compile(const char *source, Chunk *chunk) {
 	initScanner(source);
 	// I guess this is useful for future when we compile multiple chunks / functions
--- main.c
+++ main.c
@@ -7,58 +7,63 @@
 #include <string.h>
 
 static void repl() {
-  char line[1024];
-  for (;;) {
-    printf("> ");
-    if (!fgets(line, sizeof(line), stdin)) {
-      printf("\n");
-      break;
-    }
-    interpret(line);
-  }
+	char line[1024];
+	for (;;) {
+		printf("> ");
+		if (!fgets(line, sizeof(line), stdin)) {
+			printf("\n");
+			break;
+		}
+		interpret(line);
+	}
 }
 static char *readFile(const char *path) {
-  FILE *file = fopen(path, "rb");
-  if (file == NULL) {
-    fprintf(stderr, "Could not open file \"%s\".\n", path);
-    exit(74);
-  }
-  fseek(file, 0L, SEEK_END);
-  size_t fileSize = ftell(file);
-  rewind(file);
-  char *buffer = (char *)malloc(fileSize + 1);
-  if (buffer == NULL) {
-    fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
-    exit(74);
-  }
-  size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+	FILE *file = fopen(path, "rb");
+	if (file == NULL) {
+		fprintf(stderr, "Could not open file \"%s\".\n", path);
+		exit(74);
+	}
+	fseek(file, 0L, SEEK_END);
+	size_t fileSize = ftell(file);
+	rewind(file);
+	char *buffer = (char *)malloc(fileSize + 1);
+	if (buffer == NULL) {
+		fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+		exit(74);
+	}
+	size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
 
-  if (bytesRead < fileSize) {
-    fprintf(stderr, "Could not read file \"%s\".\n", path);
-    exit(74);
-  }
-  buffer[bytesRead] = '\0';
-  fclose(file);
-  return buffer;
+	if (bytesRead < fileSize) {
+		fprintf(stderr, "Could not read file \"%s\".\n", path);
+		exit(74);
+	}
+	buffer[bytesRead] = '\0';
+	fclose(file);
+	return buffer;
 }
 static void runFile(const char *path) {
-  char *source = readFile(path);
-  InterpretResult result = interpret(source);
-  free(source);
-  if (result == INTERPRET_COMPILE_ERROR)
-    exit(65);
-  if (result == INTERPRET_RUNTIME_ERROR)
-    exit(65);
+	char *source = readFile(path);
+	InterpretResult result = interpret(source);
+	free(source);
+	if (result == INTERPRET_COMPILE_ERROR)
+		exit(65);
+	if (result == INTERPRET_RUNTIME_ERROR)
+		exit(65);
 }
 int main(int argc, const char *argv[]) {
-  if (argc == 1) {
-    repl();
-  } else if (argc == 2) {
-    runFile(argv[1]);
-  } else {
-    fprintf(stderr, "Usage: clox [path]\n");
-    exit(64);
-  }
-  freeVM();
-  return 0;
+	// I initially forgot this when adding in repl
+	// https://craftinginterpreters.com/scanning-on-demand.html#spinning-up-the-interpreter
+	// And this caused a segmentation fault because we were de-referencing the vm.stackTop which was
+	// a null pointer
+	initVM();
+	if (argc == 1) {
+		repl();
+	} else if (argc == 2) {
+		runFile(argv[1]);
+	} else {
+		fprintf(stderr, "Usage: clox [path]\n");
+		exit(64);
+	}
+	freeVM();
+	return 0;
 }
