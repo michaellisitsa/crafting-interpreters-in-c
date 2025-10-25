22e2ed8722847095562d1461b77d321a3ac8f0ef
Michael Lisitsa <michael@lisitsa.dev>
2025-10-25 14:00:30 +1100

    Fix tableFindString memcmp not comparing with anything

--- table.c
+++ table.c
@@ -130,7 +130,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length, uint32_t
 			if (IS_NIL(entry->value))
 				return NULL;
 		} else if (entry->key->length == length && entry->key->hash == hash &&
-				   memcmp(entry->key->chars, chars, length)) {
+				   memcmp(entry->key->chars, chars, length) == 0) {
 			return entry->key;
 		}
 		index = (index + 1) % table->capacity;
